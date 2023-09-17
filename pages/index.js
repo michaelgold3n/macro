09328a5264624ca2d869ef3daca7841165bbcc1b
Michael Golden <67241067+michaelgold3n@users.noreply.github.com>
2023-09-17 13:29:48 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,43 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+ <Header title="September macros" />
+        <h2>IntegrityAI</h2>
+                <p className="description">
+                      Hire someone to redo web-detector accurate <code></code>
+                    </p>
+                <p className="description">
+                      Get them to Integrate the UI design <code></code>
+                    </p>
+                <p className="description">
+                     Hire someone to code extension bulk uploads <code></code>
+                    </p>
+       <h2>bot</h2>
+                 <p className="description">
+                        Check Status <code>buy computers</code>
+                      </p>
+        <h2>EmojiPulse</h2>
+                   <p className="description">
+                        Meet people at SD <code></code>
+                      </p>
+    <p className="description">
+                        Hire one as CEO with a budget + equity <code></code>
+                      </p>
+    
+    <p className="description">
+           <code></code>
         </p>
+        <h2>Golden</h2>
+    <p className="description">
+          IntegrityAI <code></code>
+        </p>
+        <h2>Nosotro</h2>
+    <p className="description">
+          IntegrityAI <code></code>
+        </p>
+
+
+        
       </main>
 
       <Footer />
